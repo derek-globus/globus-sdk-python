b2c91c97cd069bfa557ddb33e9556137066d361d
Derek Schlabach <derek@globus.org>
2024-02-08 12:39:52 -0600

    Rewrite the docs the way I like them

--- docs/examples/create_and_run_flow/manage_flow_minimal.py
+++ docs/examples/create_and_run_flow/manage_flow_minimal.py
@@ -19,13 +19,67 @@ CLIENT_ID = "61338d24-54d5-408f-a10d-66c06b59f6d2"
 NATIVE_CLIENT = globus_sdk.NativeAppAuthClient(CLIENT_ID)
 
 
-def do_login_flow():
-    NATIVE_CLIENT.oauth2_start_flow(requested_scopes=SCOPES, refresh_tokens=True)
-    authorize_url = NATIVE_CLIENT.oauth2_get_authorize_url()
-    print(f"Please go to this URL and login:\n\n{authorize_url}\n")
-    auth_code = input("Please enter the code here: ").strip()
-    tokens = NATIVE_CLIENT.oauth2_exchange_code_for_tokens(auth_code)
-    return tokens
+def main(action, flow_id, title):
+    try:
+        if action == "create":
+            if title is None:
+                print("[ERROR] create requires --title")
+                sys.exit(1)
+            create_flow(title)
+        elif action == "delete":
+            if flow_id is None:
+                print("[ERROR] delete requires --flow-id")
+                sys.exit(1)
+            delete_flow(flow_id)
+        elif action == "list":
+            list_flows()
+        else:
+            raise NotImplementedError()
+    except globus_sdk.FlowsAPIError as e:
+        print(f"API Error: {e.code} {e.message}")
+        print(e.text)
+        sys.exit(1)
+
+
+def create_flow(title):
+    flows_client = get_flows_client()
+    print(
+        flows_client.create_flow(
+            title=title,
+            definition={
+                "StartAt": "DoIt",
+                "States": {
+                    "DoIt": {
+                        "Type": "Action",
+                        "ActionUrl": "https://actions.globus.org/hello_world",
+                        "Parameters": {
+                            "echo_string": "Hello, Asynchronous World!",
+                        },
+                        "End": True,
+                    }
+                },
+            },
+            input_schema={},
+            subtitle="A flow created by the SDK tutorial",
+        )
+    )
+
+
+def delete_flow(flow_id):
+    flows_client = get_flows_client()
+    print(flows_client.delete_flow(flow_id))
+
+
+def list_flows():
+    flows_client = get_flows_client()
+    for flow in flows_client.list_flows(filter_role="flow_owner"):
+        print(f"title: {flow['title']}")
+        print(f"id: {flow['id']}")
+        print()
+
+
+def get_flows_client():
+    return globus_sdk.FlowsClient(authorizer=get_authorizer())
 
 
 def get_authorizer():
@@ -51,72 +105,19 @@ def get_authorizer():
     )
 
 
-def get_flows_client():
-    return globus_sdk.FlowsClient(authorizer=get_authorizer())
+def do_login_flow():
+    NATIVE_CLIENT.oauth2_start_flow(requested_scopes=SCOPES, refresh_tokens=True)
+    authorize_url = NATIVE_CLIENT.oauth2_get_authorize_url()
+    print(f"Please go to this URL and login:\n\n{authorize_url}\n")
+    auth_code = input("Please enter the code here: ").strip()
+    tokens = NATIVE_CLIENT.oauth2_exchange_code_for_tokens(auth_code)
+    return tokens
 
 
-def create_flow(args):
-    flows_client = get_flows_client()
-    print(
-        flows_client.create_flow(
-            title=args.title,
-            definition={
-                "StartAt": "DoIt",
-                "States": {
-                    "DoIt": {
-                        "Type": "Action",
-                        "ActionUrl": "https://actions.globus.org/hello_world",
-                        "Parameters": {
-                            "echo_string": "Hello, Asynchronous World!",
-                        },
-                        "End": True,
-                    }
-                },
-            },
-            input_schema={},
-            subtitle="A flow created by the SDK tutorial",
-        )
-    )
-
-
-def delete_flow(args):
-    flows_client = get_flows_client()
-    print(flows_client.delete_flow(args.flow_id))
-
-
-def list_flows():
-    flows_client = get_flows_client()
-    for flow in flows_client.list_flows(filter_role="flow_owner"):
-        print(f"title: {flow['title']}")
-        print(f"id: {flow['id']}")
-        print()
-
-
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("action", choices=["create", "delete", "list"])
     parser.add_argument("-f", "--flow-id", help="Flow ID for delete")
     parser.add_argument("-t", "--title", help="Name for create")
     args = parser.parse_args()
-
-    try:
-        if args.action == "create":
-            if args.title is None:
-                parser.error("create requires --title")
-            create_flow(args)
-        elif args.action == "delete":
-            if args.flow_id is None:
-                parser.error("delete requires --flow-id")
-            delete_flow(args)
-        elif args.action == "list":
-            list_flows()
-        else:
-            raise NotImplementedError()
-    except globus_sdk.FlowsAPIError as e:
-        print(f"API Error: {e.code} {e.message}")
-        print(e.text)
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
+    main(args.action, args.flow_id, args.title)
--- docs/examples/minimal_transfer_script/transfer_consent_required_reactive.py
+++ docs/examples/minimal_transfer_script/transfer_consent_required_reactive.py
@@ -3,21 +3,53 @@ import argparse
 import globus_sdk
 from globus_sdk.scopes import TransferScopes
 
-parser = argparse.ArgumentParser()
-parser.add_argument("SRC")
-parser.add_argument("DST")
-args = parser.parse_args()
-
+# Tutorial client ID - replace this with your own native client
 CLIENT_ID = "61338d24-54d5-408f-a10d-66c06b59f6d2"
 auth_client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
 
 
-# we will need to do the login flow potentially twice, so define it as a
-# function
-#
-# we default to using the Transfer "all" scope, but it is settable here
-# look at the ConsentRequired handler below for how this is used
+def main(src, dst):
+    # get an initial client to try with, which requires a login flow
+    transfer_client = login_and_get_transfer_client()
+
+    # create a Transfer task consisting of one or more items
+    task_data = globus_sdk.TransferData(source_endpoint=src, destination_endpoint=dst)
+    task_data.add_item(
+        "/share/godata/file1.txt",  # source
+        "/~/example-transfer-script-destination.txt",  # dest
+    )
+
+    try:
+        do_submit(transfer_client, task_data)
+    except globus_sdk.TransferAPIError as err:
+        # if the error is something other than consent_required, reraise it,
+        # exiting the script with an error message
+        if not err.info.consent_required:
+            raise
+
+        # we now know that the error is a ConsentRequired
+        # print an explanatory message and do the login flow again
+        print(
+            "Encountered a ConsentRequired error.\n"
+            "You must login a second time to grant consents.\n\n"
+        )
+        transfer_client = login_and_get_transfer_client(
+            scopes=err.info.consent_required.required_scopes
+        )
+
+        # finally, try the submission a second time, this time with no error
+        # handling
+        do_submit(transfer_client)
+
+
 def login_and_get_transfer_client(*, scopes=TransferScopes.all):
+    """
+    we will need to do the login flow potentially twice, so define it as a
+    function
+
+    we default to using the Transfer "all" scope, but it is settable here
+    look at the ConsentRequired handler below for how this is used
+    """
     auth_client.oauth2_start_flow(requested_scopes=scopes)
     authorize_url = auth_client.oauth2_get_authorize_url()
     print(f"Please go to this URL and login:\n\n{authorize_url}\n")
@@ -32,46 +64,18 @@ def login_and_get_transfer_client(*, scopes=TransferScopes.all):
     )
 
 
-# get an initial client to try with, which requires a login flow
-transfer_client = login_and_get_transfer_client()
-
-# create a Transfer task consisting of one or more items
-task_data = globus_sdk.TransferData(
-    source_endpoint=args.SRC, destination_endpoint=args.DST
-)
-task_data.add_item(
-    "/share/godata/file1.txt",  # source
-    "/~/example-transfer-script-destination.txt",  # dest
-)
-
-
-# define the submission step -- we will use it twice below
-def do_submit(client):
+def do_submit(client, task_data):
+    """
+    define the submission step -- we will use it twice below
+    """
     task_doc = client.submit_transfer(task_data)
     task_id = task_doc["task_id"]
     print(f"submitted transfer, task_id={task_id}")
 
 
-# try to submit the task
-# if it fails, catch the error...
-try:
-    do_submit(transfer_client)
-except globus_sdk.TransferAPIError as err:
-    # if the error is something other than consent_required, reraise it,
-    # exiting the script with an error message
-    if not err.info.consent_required:
-        raise
-
-    # we now know that the error is a ConsentRequired
-    # print an explanatory message and do the login flow again
-    print(
-        "Encountered a ConsentRequired error.\n"
-        "You must login a second time to grant consents.\n\n"
-    )
-    transfer_client = login_and_get_transfer_client(
-        scopes=err.info.consent_required.required_scopes
-    )
-
-    # finally, try the submission a second time, this time with no error
-    # handling
-    do_submit(transfer_client)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("SRC")
+    parser.add_argument("DST")
+    args = parser.parse_args()
+    main(args.SRC, args.DST)
